7f6c889388dd2de7661479c96afa670ac04477fe
ixx <ixxcome123@163.com>
2015-08-05 16:15:26 +0800

    初始化项目

---
+++ ixxWaterFlow/ixxWaterFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ixxWaterFlow
+//
+//  Created by apple on 15/8/5.
+//  Copyright (c) 2015年 www.incoshare.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ixxWaterFlow/ixxWaterFlow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ixxWaterFlow
+//
+//  Created by apple on 15/8/5.
+//  Copyright (c) 2015年 www.incoshare.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
